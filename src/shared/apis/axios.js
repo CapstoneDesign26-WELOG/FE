371aedde72236b5328d06880ad4968824f79b9ab
name <rlaclrk3@gmail.com>
2026-05-05 01:50:12 +0900

    chore: axios 및 tanstack query 초기 세팅

---
+++ src/shared/apis/axios.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+axiosInstance.interceptors.response.use(
+  (res) => res,
+  (error) => Promise.reject(error),
+);
