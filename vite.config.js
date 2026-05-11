ed6bf9e086ed740b1833a76ce3095a4b7337f250
bongtta <kce0419@naver.com>
2026-05-11 15:33:11 +0900

    feat: 폰트 적용

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 const dirname = path.dirname(fileURLToPath(import.meta.url));
