3ea59f51271fa174af20d569fa759c77d8339b00
bongtta <kce0419@naver.com>
2026-05-12 06:06:00 +0900

    feat: 바텀 네비게이션 구현

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,7 @@
-import Header from '@/shared/components/header/header';
-
-const Home = () => <div>개인 게시판 페이지</div>;
+const Home = () => (
+  <>
+    <div>개인 게시판 페이지</div>
+  </>
+);
 
 export default Home;
