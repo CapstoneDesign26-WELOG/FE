1c4fef7264d9142272c5fe858f4033b957abd309
CHAEEUN KIM <154000318+bongtta@users.noreply.github.com>
2026-05-12 22:31:02 +0900

    Refactor Layout component to improve navigation logic

--- src/shared/routes/layout.jsx
+++ src/shared/routes/layout.jsx
@@ -1,31 +1,22 @@
 import { Outlet, matchPath, useLocation } from 'react-router-dom';
 import BottomNavigation from '../components/bottom-navigation/bottom-navigation';
 import { ROUTES } from './routes-config';
-  
+
 const HIDDEN_NAV_ROUTES = [ROUTES.NOTIFICATION, ROUTES.CREATE];
-  
+
 const Layout = () => {
   const { pathname } = useLocation();
+
   const hideNav = HIDDEN_NAV_ROUTES.includes(pathname);
   const isDetailPage = matchPath(ROUTES.DETAIL(), pathname);
-
-  return (
-    <div className="flex min-h-screen flex-col">
-      <main className="relative flex flex-1 flex-col">
-        <Outlet />
-      </main>
-
-      {!isDetailPage && <BottomNavigation />}
-    </div>
-  );
-};
 
   return (
     <div className="flex min-h-screen flex-col">
       <main className="relative flex-1 flex flex-col">
         <Outlet />
       </main>
-      {!hideNav && <BottomNavigation />}
+
+      {!hideNav && !isDetailPage && <BottomNavigation />}
     </div>
   );
 };
