13e1f8d3a09c2789e880a0ff15db2441f183f2e3
bongtta <kce0419@naver.com>
2026-05-12 15:36:10 +0900

    chore: 목데이터 생성

---
+++ src/pages/home/constants/mock-posts.js
@@ -0,0 +1,26 @@
+export const MOCK_POSTS = [
+  {
+    id: 1,
+    title: '1교시 드랍할까? 통학 3시간 너무 힘들어ㅜㅜ',
+    commentCount: 5,
+    createdAt: '3분 전',
+  },
+  {
+    id: 2,
+    title: '팀플 너무 힘든데 어떡하지... 팀원 중에 한 명이 연락이 안 돼',
+    commentCount: 2,
+    createdAt: '17분 전',
+  },
+  {
+    id: 3,
+    title: '좋아하는 사람이 생겼는데 고백할까 말까',
+    commentCount: 9,
+    createdAt: '3일 전',
+  },
+  {
+    id: 4,
+    title: '취준생인데 자소서 쓰다가 멘붕 왔어 ㅠㅠ',
+    commentCount: 15,
+    createdAt: '7일 전',
+  },
+];
