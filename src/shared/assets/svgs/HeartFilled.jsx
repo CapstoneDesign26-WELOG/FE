c5536db924ebe1b6902570088295bc9954da5ae8
bongtta <kce0419@naver.com>
2026-05-23 03:48:03 +0900

    chore: svgr 실행

---
+++ src/shared/assets/svgs/HeartFilled.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+const SvgHeartFilled = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 16 14"
+    {...props}
+  >
+    <path
+      fill="#FA4250"
+      d="M8.645 13.31a1.497 1.497 0 0 1-2.048-.008l-.084-.077C2.516 9.535-.097 7.118.003 4.103.048 2.783.71 1.516 1.785.77c2.01-1.399 4.492-.746 5.832.854C8.957.024 11.44-.636 13.45.77c1.074.746 1.736 2.012 1.782 3.333.106 3.015-2.513 5.431-6.51 9.137z"
+    />
+  </svg>
+);
+export default SvgHeartFilled;
