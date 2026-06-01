6534b085da144d9bae454feecbf7419d10378af2
bongtta <kce0419@naver.com>
2026-06-01 13:34:41 +0900

    feat: 제목란 추가

--- src/pages/create/create.jsx
+++ src/pages/create/create.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import Header from '@/shared/components/header/header';
 
 const Create = () => {
+  const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const navigate = useNavigate();
 
@@ -18,6 +19,17 @@ const Create = () => {
         onBackClick={() => navigate(-1)}
         onRightClick={handleComplete}
       />
+
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="제목을 입력해주세요."
+        className="body_16_b px-[2rem] py-[1.6rem] text-gray-black outline-none placeholder:text-gray-500"
+      />
+
+      <div className="mx-[2rem] h-[0.1rem] bg-gray-300" />
+
       <textarea
         className="body_16_m flex-1 resize-none p-[2rem] text-gray-black outline-none placeholder:text-gray-500"
         placeholder="고민을 자유롭게 적어보세요..."
